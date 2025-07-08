c7a3a72e6eefece856cebb97b46848e6408582da
Sinem Ertural <sinemertural1655@gmail.com>
2025-07-08 10:06:51 +0300

    chore: Initialize movies application

---
+++ filmler_uygulamasi/lib/data/entity/filmler.dart
@@ -0,0 +1,13 @@
+class Filmler{
+  int id;
+  String ad;
+  String resim;
+  int fiyat;
+
+  Filmler({
+    required this.id,
+    required this.ad,
+    required this.resim,
+    required this.fiyat
+  });
+}
